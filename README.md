dd0e598aa6cb9a3e3f21e6d080c9d52eaedd6fe8
kelvin-nyale <kelvinnyale780@gmail.com>
2025-04-11 16:23:45 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dantic0-portifolio-
---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer", "Full-Stack Developer", "Cloud Architect", "DevOps Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
